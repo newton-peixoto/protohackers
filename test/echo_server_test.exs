68853177b2a277a441f545e885e88d34c97c75dc
Newton <newton@newton-2.local>
2023-07-09 09:37:03 -0300

    feat: change code style and also test multiple requests

--- test/echo_server_test.exs
+++ test/echo_server_test.exs
@@ -11,4 +11,19 @@ defmodule EchoServerTest do
 
     assert :gen_tcp.recv(socket, 0) == {:ok, "foofoo2"}
   end
+
+  test "handles multiple concurrent connections" do
+    tasks =
+      for _ <- 1..4 do
+        Task.async(fn ->
+          {:ok, socket} = :gen_tcp.connect(~c"localhost", 5001, mode: :binary, active: false)
+          assert :gen_tcp.send(socket, "foo") == :ok
+          assert :gen_tcp.send(socket, "bar") == :ok
+          :gen_tcp.shutdown(socket, :write)
+          assert :gen_tcp.recv(socket, 0, 5000) == {:ok, "foobar"}
+        end)
+      end
+
+    Enum.each(tasks, &Task.await/1)
+  end
 end
